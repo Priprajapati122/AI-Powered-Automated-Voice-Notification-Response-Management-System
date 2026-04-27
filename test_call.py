7b4f501fa68b3e4b05cc24298b83cb8992701417
Priprajapati122 <priprajapati122@gmail.com>
2026-04-27 19:47:02 +0530

    Obscure Twilio credentials in test_call.py
    Replaced sensitive Twilio credentials with placeholders.

--- test_call.py
+++ test_call.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = "AC5d486edbea3b01d871459ecc6ab2a509"
-auth_token = "b6dd086fc852e89276e20b0a9f789244"
+account_sid = "#########"
+auth_token = "##########"
 
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
-    to="+916392270722",
-    from_="+12603005044",
-    url="http://demo.twilio.com/docs/voice.xml"
+    to="#########",
+    from_="#########",
+    url="#############"
 )
 
 print("Call SID:", call.sid)
